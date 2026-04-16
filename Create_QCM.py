3a0ed7ce238ab7496a9fa68a98987e0199c39985
MarioTheBoss <rayan.kachouri@gmail.com>
2026-04-16 09:46:37 +0200

    Update Create_QCM.py

--- Create_QCM.py
+++ Create_QCM.py
@@ -1,1 +1,26 @@
+### IMPORTS ###
+from pygame import *
 
+
+
+
+
+### SETTING ###
+init()
+WIDTH = 600
+HEIGHT = 300
+screen = dispaly.set_mode((WIDTH, HEIGHT))
+display.set_caption("Faire son QCM facilement")
+
+state = "Name"
+
+
+
+
+### DISPLAY ###
+running = True
+while running:
+  for event in event.get():
+    if event.type == QUIT:
+      running = False
+    
